487cd0ced290eae6224879fd457fb1e78150bbc3
Rajat <varshneyrajat999@gmail.com>
2023-09-17 00:54:36 +0530

    first commit

---
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package com.example.demo.service.impl;
+
+import java.util.List;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+
+public class UserServiceImpl  implements UserService{
+
+	@Override
+	public List<User> getDetail() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
